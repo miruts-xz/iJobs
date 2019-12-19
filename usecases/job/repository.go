1fb547a94a4d2259109f511a15a31594dce4a02f
miruts <miruts.hadush@aait.edu.et>
2019-12-19 13:40:37 +0300

    separating usecase implementation

--- usecases/job/repository.go
+++ usecases/job/repository.go
@@ -4,6 +4,7 @@ import "github.com/miruts/iJobs/entity"
 
 type JobRepository interface {
 	Jobs() ([]entity.Job, error)
+	JobsOfCategory(cat_id int) ([]entity.Job, error)
 	Job(id int) (entity.Job, error)
 	UpdateJob(job entity.Job) error
 	DeleteJob(id int) error
--- usecases/job/service/job_service.go
+++ usecases/job/service/job_service.go
@@ -1,1 +1,43 @@
 package service
+
+import (
+	"github.com/miruts/iJobs/entity"
+	"github.com/miruts/iJobs/usecases/job/repository"
+)
+
+type JobServices struct {
+	handler *repository.JobRepository
+}
+
+func NewJobService(handler *repository.JobRepository) *JobServices {
+	return &JobServices{handler: handler}
+}
+
+func (jobService *JobServices) Jobs() ([]entity.Job, error) {
+	return jobService.handler.Jobs()
+}
+
+//Returns all jobs under a specific category
+func (jobService *JobServices) JobsOfCategory(cat_id int) ([]entity.Job, error) {
+	return jobService.handler.JobsOfCategory(cat_id)
+}
+
+//Returns a job given an its id
+func (jobService *JobServices) Job(id int) (entity.Job, error) {
+	return jobService.handler.Job(id)
+}
+
+//Updates a job given the udpated job object
+func (jobService *JobServices) UpdateJob(job entity.Job) error {
+	return jobService.UpdateJob(job)
+}
+
+//Deletes a job given its id
+func (jobService *JobServices) DeleteJob(id int) error {
+	return jobService.handler.DeleteJob(id)
+}
+
+//Adds a job to the database
+func (jobService *JobServices) StoreJob(job entity.Job) error {
+	return jobService.StoreJob(job)
+}
