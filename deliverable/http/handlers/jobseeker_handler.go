3ec368b10d5fd5ad6c759965f261316fa182f581
miruts <miruts.hadush@aait.edu.et>
2020-01-01 12:44:13 +0300

    job seeker handler utils

--- deliverable/http/handlers/jobseeker_handler.go
+++ deliverable/http/handlers/jobseeker_handler.go
@@ -5,9 +5,14 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/miruts/iJobs/entity"
 	"github.com/miruts/iJobs/usecases/jobseeker"
+	"github.com/miruts/iJobs/util"
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 )
 
@@ -42,7 +47,7 @@ func NewJobseekerHandler(tmpl *template.Template, jss jobseeker.JobseekerService
 	email 		- email
 	gender 		- gender
 */
-func JobseekerRegister(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (jsh *JobseekerHandler) JobseekerRegister(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	err := r.ParseForm()
 	err = r.ParseMultipartForm(1024)
 	if err != nil {
@@ -65,7 +70,7 @@ func JobseekerRegister(w http.ResponseWriter, r *http.Request, ps httprouter.Par
 	jobseeker.Fullname = fname + " " + lname
 	// todo process, make it secure and store user entered password
 	pswd := ps.ByName("pswd")
-	if !hasvalue(pswd) {
+	if !hasvalue(pswd) || len(pswd) < 4 {
 
 	}
 	hashed, err := bcrypt.GenerateFromPassword([]byte(pswd), bcrypt.DefaultCost)
@@ -78,40 +83,105 @@ func JobseekerRegister(w http.ResponseWriter, r *http.Request, ps httprouter.Par
 	jobseeker.Password = hashedpwsd
 	// todo process and store user entered work experience
 	wrkexp := ps.ByName("wrkexp")
-	if !hasvalue(wrkexp) {
-	}
 	wrkexpint, err := strconv.Atoi(wrkexp)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return
 	}
+	if !hasvalue(wrkexp) || wrkexpint < 0 {
+	}
 	jobseeker.WorkExperience = int64(wrkexpint)
 
-	// todo process and store selected interested job categories
-	intjobcat := r.Form["intjobcat"]
-	if !hasvalue(intjobcat) {
-	}
-	for _, v := range intjobcat {
-		id := v
-
-	}
-	// todo process and store user entered
-	portf := r.Form["portf"]
+	// todo process and store user entered portfolio
+	portf := r.FormValue("portf")
 	if !hasvalue(portf) {
 
 	}
+	jobseeker.Portfolio = portf
+
 	// todo process and store user entered profile picture
 	propic, fh, err := r.FormFile("propic")
 	if err != nil {
 
 	}
+	path, err := os.Getwd()
+	fmt.Println(path)
+	path = path[:len(path)-25]
+	fmt.Println(path)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return false
+	}
+	path = filepath.Join(path, "ui", "assets", "jsdata", uname, "pp")
+	err = os.MkdirAll(path, 0644)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return false
+	}
+	path = filepath.Join(path, fh.Filename)
+	written := util.SaveFile(propic, path)
+	if !written {
+
+	}
+
 	// todo process and store user entered cv
 	cv, fh, err := r.FormFile("cv")
 	if err != nil {
 
 	}
+	path, err = os.Getwd()
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	fmt.Println(path)
+	path = path[:len(path)-25]
+	fmt.Println(path)
+	path = filepath.Join(path, "ui", "assets", "jsdata", uname, "cv")
+	err = os.MkdirAll(path, 0644)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	path = filepath.Join(path, fh.Filename)
+	cvWritten := util.SaveFile(cv, path)
+	if !cvWritten {
 
-	jobseeker := entity.JobSeeker{}
+	}
+
+	err = jsh.jsSrv.StoreJobSeeker(jobseeker)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	js, err := jsh.jsSrv.JobSeekers()
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	var jsid int64
+	for _, v := range js {
+		if v.Username == uname {
+			jsid = v.ID
+			break
+		}
+	}
+
+	// todo process and store selected interested job categories
+	intjobcat := r.Form["intjobcat"]
+	if !hasvalue(intjobcat) {
+	}
+	for v := range intjobcat {
+		jcid := v
+		err = jsh.jsSrv.AddIntCategory(int(jsid), jcid)
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+			return
+		}
+	}
+}
+
+func WriteProfileToLocal(propic multipart.File, fh string, uname string) bool {
 
 }
 func hasvalue(value interface{}) bool {
