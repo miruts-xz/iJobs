667cf57ca3c787522b6564ec78d37260894e438c
miruts <miruts.hadush@aait.edu.et>
2020-01-09 09:03:45 +0300

    branches merged

--- usecases/jobseeker/repository/psql_jobseeker.go
+++ usecases/jobseeker/repository/psql_jobseeker.go
@@ -18,14 +18,14 @@ func NewJobseekerRepositoryImpl(jsr *sql.DB) *JobseekerRepositoryImpl {
 }
 
 // JobSeekers retrieves and returns all jobseekers
-func (jsr *JobseekerRepositoryImpl) JobSeekers() ([]entity.JobSeeker, error) {
+func (jsr *JobseekerRepositoryImpl) JobSeekers() ([]entity.Jobseeker, error) {
 	query := "select * from jobseekers"
 	rows, err := jsr.conn.Query(query)
 	if err != nil {
 		return nil, errors.New("unable to retrieve jobseekers")
 	}
-	var jobSeekers []entity.JobSeeker
-	var jobSeeker entity.JobSeeker
+	var jobSeekers []entity.Jobseeker
+	var jobSeeker entity.Jobseeker
 	for rows.Next() {
 		if err := rows.Scan(&jobSeeker.ID, &jobSeeker.Username, &jobSeeker.Fullname, &jobSeeker.Email, &jobSeeker.Phone, &jobSeeker.Password, &jobSeeker.Profile, &jobSeeker.WorkExperience, &jobSeeker.CV, &jobSeeker.Portfolio, &jobSeeker.EmpStatus, &jobSeeker.Gender, &jobSeeker.Age); err != nil {
 			return nil, errors.New("unable to retrieve jobseekers")
@@ -36,9 +36,9 @@ func (jsr *JobseekerRepositoryImpl) JobSeekers() ([]entity.JobSeeker, error) {
 }
 
 // JobSeeker return a jobseeker with given id
-func (jsr *JobseekerRepositoryImpl) JobSeeker(id int) (entity.JobSeeker, error) {
+func (jsr *JobseekerRepositoryImpl) JobSeeker(id int) (entity.Jobseeker, error) {
 	query := "select * from jobseekers where id = $1"
-	var jobSeeker entity.JobSeeker
+	var jobSeeker entity.Jobseeker
 	err := jsr.conn.QueryRow(query, id).Scan(&jobSeeker.ID, &jobSeeker.Username, &jobSeeker.Fullname, &jobSeeker.Email, &jobSeeker.Phone, &jobSeeker.Password, &jobSeeker.Profile, &jobSeeker.WorkExperience, &jobSeeker.CV, &jobSeeker.Portfolio, &jobSeeker.EmpStatus, &jobSeeker.Gender, &jobSeeker.Age)
 	if err != nil {
 		return jobSeeker, errors.New("unable to retrieve jobseeker")
@@ -47,7 +47,7 @@ func (jsr *JobseekerRepositoryImpl) JobSeeker(id int) (entity.JobSeeker, error) 
 }
 
 // UpdateJobSeeker updates a given jobseeker
-func (jsr *JobseekerRepositoryImpl) UpdateJobSeeker(js *entity.JobSeeker) (*entity.JobSeeker, error) {
+func (jsr *JobseekerRepositoryImpl) UpdateJobSeeker(js *entity.Jobseeker) (*entity.Jobseeker, error) {
 	query := "update jobseekers set id=$1, username=$2, fullname=$3, email=$4, phone=$5, password=$6, profile=$7, work_exp=$8, cv=$9, portfolio=$10, emp_status=$11, gender=$12, age=$13"
 	_, err := jsr.conn.Exec(query, js.ID, js.Username, js.Fullname, js.Email, js.Phone, js.Password, js.Profile, js.WorkExperience, js.CV, js.Portfolio, js.EmpStatus, js.Gender, js.Age)
 	if err != nil {
@@ -57,7 +57,7 @@ func (jsr *JobseekerRepositoryImpl) UpdateJobSeeker(js *entity.JobSeeker) (*enti
 }
 
 // DeleteJobSeeker deletes a jobseeker with a given id
-func (jsr *JobseekerRepositoryImpl) DeleteJobSeeker(id int) (entity.JobSeeker, error) {
+func (jsr *JobseekerRepositoryImpl) DeleteJobSeeker(id int) (entity.Jobseeker, error) {
 	js, err := jsr.JobSeeker(id)
 	if err != nil {
 		return js, nil
@@ -103,7 +103,7 @@ func (jsr *JobseekerRepositoryImpl) JsCategories(id int) ([]entity.Category, err
 }
 
 // StoreJobSeeker stores new jobseeker
-func (jsr *JobseekerRepositoryImpl) StoreJobSeeker(js *entity.JobSeeker) (*entity.JobSeeker, error) {
+func (jsr *JobseekerRepositoryImpl) StoreJobSeeker(js *entity.Jobseeker) (*entity.Jobseeker, error) {
 	query := "insert into jobseekers (username, fullname, email, phone, password, profile, work_exp, cv, portfolio, emp_status, gender, age) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)"
 	_, err := jsr.conn.Exec(query, js.ID, js.Username, js.Fullname, js.Email, js.Phone, js.Password, js.Profile, js.WorkExperience, js.CV, js.Portfolio, js.EmpStatus, js.Gender, js.Age)
 	if err != nil {
